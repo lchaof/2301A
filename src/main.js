79face88fd0c3aa1ff6e2dc28a1d33e2febe26ef
lchaof <2640371952@qq.com>
2023-12-01 10:48:44 +0800

    feat: 新增axios配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import http from "./util/httpRequest"
 
 import { Button, Select,MessageBox,Loading } from 'element-ui';
 
@@ -9,9 +10,11 @@ Vue.config.productionTip = false;
 
 Vue.use(Button)
 Vue.use(Select)
-Vue.use(MessageBox)
-
-Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$msgbox = MessageBox
+Vue.prototype.$alert = MessageBox.alert
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$prompt = MessageBox.prompt
+Vue.prototype.$http = http
 
 new Vue({
   router,
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+		<Helloword />
   </div>
 </template>
+
+<script>
+import Helloword from "$components/HelloWorld.vue"
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    Helloword
+  },
+  created() {
+    console.log(process.env)
+    this.$http
+      .post(this.$http.addURL("/sys/login"), {
+        mobile: "17764049967",
+        password: 123456
+      })
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
+</script>
